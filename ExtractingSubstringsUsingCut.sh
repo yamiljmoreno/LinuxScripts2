860f2d021d7f49bd2b09f6e1262c0fad5b21d87f
Yamil Josue Moreno <114109636+yamiljmoreno@users.noreply.github.com>
2022-10-31 22:46:06 -0600

    Add files via upload

---
+++ ExtractingSubstringsUsingCut.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Str="Learn Bash Commands from UbuntuPit"
+#subStr=${Str:0:20}
+
+subStr=$(echo $Str| cut -d ' ' -f 1-3)
+echo $subStr
---
+++ TheWhileLoop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+i=0
+
+while [ $i -le 2 ]
+do
+echo Number: $i
+((i++))
+done
+while [ condition ]
+do
+commands 1
+commands n
+done
